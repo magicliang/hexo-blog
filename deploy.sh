c39fb448ac836a1dae3bc6e08bf011e25640d416
magicliang <liangchuan@meituan.com>
2020-03-29 11:52:52 +0800

    change blog

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,18 @@ git pull
 # 以后用post文件夹来存储附件
 # ./delete_all_post_folders.sh
 
-echo "begin to scan sensitive contents:\n"
+echo "begin to scan sensitive contents:"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
 grep -rn --color "\.sankuai\.\|\.meituan\." ./source/_posts
-
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
 #if echo "$OUTPUT" | grep -q "(Status:\s200)"; then
 #    echo "MATCH"
 #fi
